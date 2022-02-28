f3f6accb8756d5cbf0ecc080fcc61654732886a4
Michael <biichle93@gmail.com>
2022-02-28 15:33:59 -0500

    Added detailed view for movies when a user clicks on an item in the recyclerview. Uses YoutubeAndroidPlayer Api for retrieving trailers from YouTube. Added rating bar that displays current average rating for the movie

--- app/src/main/java/com/example/flixster/Movie.kt
+++ app/src/main/java/com/example/flixster/Movie.kt
@@ -1,16 +1,23 @@
 package com.example.flixster
 
 import android.content.res.Configuration
+import android.os.Parcelable
+import kotlinx.parcelize.IgnoredOnParcel
+import kotlinx.parcelize.Parcelize
 import org.json.JSONArray
 
+@Parcelize
 data class Movie (
     val movieID: Int,
     val overview: String,
+    val voteAverage: Double,
     private val posterPath: String,
     val title: String,
     private val posterPathL: String
-){
+): Parcelable{
+    @IgnoredOnParcel
     val posterImageURL = "https://image.tmdb.org/t/p/w342/$posterPath"
+    @IgnoredOnParcel
     val posterImageLURL = "https://image.tmdb.org/t/p/w342/$posterPathL"
     companion object{
         fun fromJsonArray(jsonArray: JSONArray): List<Movie>{
@@ -21,6 +28,7 @@ data class Movie (
                     Movie(
                         moviejson.getInt("id"),
                         moviejson.getString("overview"),
+                        moviejson.getDouble("vote_average"),
                         moviejson.getString("poster_path"),
                         moviejson.getString("title"),
                         moviejson.getString("backdrop_path")
--- app/src/main/java/com/example/flixster/MovieAdapter.kt
+++ app/src/main/java/com/example/flixster/MovieAdapter.kt
@@ -1,14 +1,18 @@
 package com.example.flixster
 
 import android.content.Context
+import android.content.Intent
 import android.content.res.Configuration
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+
+const val MOVIE_EXTRA = "MOVIE_EXTRA"
 
 class MovieAdapter(private val context: Context, private val movies: List<Movie>)
     : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
@@ -31,14 +35,17 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
 
     override fun getItemCount() = movies.size
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
-        private val tvOverview = itemView.findViewById<TextView>(R.id.tvOverview)
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener{
+        private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitleDetail)
+        private val tvOverview = itemView.findViewById<TextView>(R.id.tvOverviewDetail)
         private val ivPoster = itemView.findViewById<ImageView>(R.id.ivPoster)
-
         private val ivPosterL = itemView.findViewById<ImageView>(R.id.ivPosterL)
         private val tvOverviewL = itemView.findViewById<TextView>(R.id.tvOverviewL)
         private val tvTitleL = itemView.findViewById<TextView>(R.id.tvTitleL)
+
+        init{
+            itemView.setOnClickListener(this)
+        }
 
         fun bind(movie: Movie){
             val orientation = context.resources.configuration.orientation
@@ -51,6 +58,16 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
                 tvOverview.text = movie.overview
                 Glide.with(context).load(movie.posterImageURL).into(ivPoster)
             }
+        }
+
+        override fun onClick(v: View?) {
+            // get notified for which movie was clicked
+            val movieClicked = movies[adapterPosition]
+            Toast.makeText(context, movieClicked.title + " was clicked", Toast.LENGTH_SHORT).show()
+            // use intents to launch new activity
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra(MOVIE_EXTRA, movieClicked)
+            context.startActivity(intent)
         }
     }
     }
